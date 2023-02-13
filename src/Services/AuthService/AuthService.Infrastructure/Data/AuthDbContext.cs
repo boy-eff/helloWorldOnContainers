ce901ed6ce69cc1018a4806e3cffd5fc17687dca
boy-eff <boyev.danila@gmail.com>
2023-02-13 16:59:01 +0300

    Add migrations

--- src/Services/AuthService/AuthService.Infrastructure/Data/AuthDbContext.cs
+++ src/Services/AuthService/AuthService.Infrastructure/Data/AuthDbContext.cs
@@ -1,10 +1,18 @@
 using AuthService.Domain.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Infrastructure.Data
 {
     public class AuthDbContext: IdentityDbContext<AppUser>
     {
-        
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
