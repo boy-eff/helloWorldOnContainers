9df6a2e9c9706dc2bcc4a6e8df057af641d537fe
boy-eff <boyev.danila@gmail.com>
2023-02-20 16:43:37 +0300

    Add dtos

---
+++ src/Services/Words/Words.BusinessAccess/Dtos/WordCollectionDto.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Enums;
+
+namespace Words.BusinessAccess.Dtos;
+
+public class WordCollectionDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public EnglishLevel EnglishLevel { get; set; }
+}
