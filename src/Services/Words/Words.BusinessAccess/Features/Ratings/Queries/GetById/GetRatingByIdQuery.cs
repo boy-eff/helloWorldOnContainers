83d12ac73755244341b17dad499cb4514d791c34
boy-eff <boyev.danila@gmail.com>
2023-03-02 13:35:13 +0300

    Add rating mediatR requests

---
+++ src/Services/Words/Words.BusinessAccess/Features/Ratings/Queries/GetById/GetRatingByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Words.BusinessAccess.Dtos;
+using Words.BusinessAccess.Dtos.CollectionRating;
+
+namespace Words.BusinessAccess.Features.Ratings.Queries.GetById;
+
+public record GetRatingByIdQuery(int Id) : IRequest<CollectionRatingResponseDto>;
