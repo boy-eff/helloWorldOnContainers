b8500a8dd8d4a1f13019ccfcf915570179440d35
boy-eff <boyev.danila@gmail.com>
2023-03-06 00:34:11 +0300

    Setup RabbitMQ in Identity service

---
+++ src/Services/Identity/Identity.Infrastructure/Extensions/MassTransitExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Infrastructure.Extensions;
+
+public static class MassTransitExtensions
+{
+    public static void ConfigureMassTransit(this IServiceCollection services, ConfigurationManager config)
+    {
+        services.AddMassTransit(x =>
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var host = config["RabbitMQ:Host"];
+            var virtualHost = config["RabbitMQ:VirtualHost"];
+            var username = config["RabbitMQ:Username"];
+            var password = config["RabbitMQ:Password"];
+
+            x.AddConsumers(assembly);
+
+            x.UsingRabbitMq((context, cfg) => {
+                cfg.Host(host, virtualHost, h => {
+                    h.Username(username);
+                    h.Password(password);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+    }
+}
