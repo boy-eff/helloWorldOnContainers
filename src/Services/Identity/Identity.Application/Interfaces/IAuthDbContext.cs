ee7672405ff36d79fb615ddb3ddf8689759d0d97
boy-eff <boyev.danila@gmail.com>
2023-03-06 00:35:30 +0300

    Add interface for AuthDbContext to access from Application layer

---
+++ src/Services/Identity/Identity.Application/Interfaces/IAuthDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Identity.Application.Interfaces;
+
+public interface IAuthDbContext : IDisposable
+{
+    public DatabaseFacade Database { get; }
+    Task<int> SaveChangesAsync();
+}
