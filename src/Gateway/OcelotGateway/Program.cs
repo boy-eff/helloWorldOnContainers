090c99e3f26d4d1eb97f69459298260ae1c9749f
boy-eff <boyev.danila@gmail.com>
2023-03-16 17:53:19 +0300

    Add docker support

--- src/Gateway/OcelotGateway/Program.cs
+++ src/Gateway/OcelotGateway/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile(builder.Configuration["Ocelot:JsonFile"], optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
-
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
