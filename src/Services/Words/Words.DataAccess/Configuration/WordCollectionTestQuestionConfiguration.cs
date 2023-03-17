47742025903978add9834da4f341d4d81d6ae359
boy-eff <boyev.danila@gmail.com>
2023-03-17 20:21:57 +0300

    Add constraints to nvarchar

---
+++ src/Services/Words/Words.DataAccess/Configuration/WordCollectionTestQuestionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Words.DataAccess.Models;
+
+namespace Words.DataAccess.Configuration;
+
+public class WordCollectionTestQuestionConfiguration : IEntityTypeConfiguration<WordCollectionTestQuestion>
+{
+    public void Configure(EntityTypeBuilder<WordCollectionTestQuestion> builder)
+    {
+        builder.Property(x => x.CorrectAnswer)
+            .HasMaxLength(50);
+
+        builder.Property(x => x.UserAnswer)
+            .HasMaxLength(50);
+    }
+}
