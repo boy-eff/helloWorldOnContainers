5ebd90e370c51acdd248ccd3593185616604d46f
boy-eff <boyev.danila@gmail.com>
2023-03-21 20:31:49 +0300

    Follow naming conventions for private constants

--- src/Services/Words/Words.BusinessAccess/Extensions/DateTimeExtensions.cs
+++ src/Services/Words/Words.BusinessAccess/Extensions/DateTimeExtensions.cs
@@ -2,18 +2,18 @@
 
 public static class DateTimeExtensions
 {
-    private const int _marchMonthNumber = 3;
-    private const int _firstDayInMonth = 1;
-    private const int _februaryMonthNumber = 2;
-    private const int _februaryLastDayInMonthLeap = 29;
+    private const int s_marchMonthNumber = 3;
+    private const int s_firstDayInMonth = 1;
+    private const int s_februaryMonthNumber = 2;
+    private const int s_februaryLastDayInMonthLeap = 29;
     
     public static bool IsFirstOfMarch(this DateTime dateTime)
     {
-        return dateTime.Day == _firstDayInMonth && dateTime.Month == _marchMonthNumber;
+        return dateTime.Day == s_firstDayInMonth && dateTime.Month == s_marchMonthNumber;
     }
     
     public static bool IsLastDayOfLeapFebruary(this DateTimeOffset dateTime)
     {
-        return dateTime.Day == _februaryLastDayInMonthLeap && dateTime.Month == _februaryMonthNumber;
+        return dateTime.Day == s_februaryLastDayInMonthLeap && dateTime.Month == s_februaryMonthNumber;
     }
 }
