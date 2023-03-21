545ed55edecfaf5a4874f7e6dfea73d86efdfbaa
boy-eff <boyev.danila@gmail.com>
2023-03-21 13:09:36 +0300

    Refactor DictionaryController.cs

--- src/Services/Words/Words.WebAPI/Controllers/DictionaryController.cs
+++ src/Services/Words/Words.WebAPI/Controllers/DictionaryController.cs
@@ -26,11 +26,11 @@ public class DictionaryController: ControllerBase
     /// <response code="200">Returns added word id</response>
     /// <response code="404">If word is not found</response>
     /// <response code="401">If user is not authenticated</response>
-    [HttpPost]
+    [HttpPost("words/{wordId:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<int>> AddWordToDictionaryAsync([FromBody]int wordId)
+    public async Task<ActionResult<int>> AddWordToDictionaryAsync(int wordId)
     {
         var command = new AddWordToDictionaryCommand(wordId);
         var result = await _mediator.Send(command, CancellationToken.None);
