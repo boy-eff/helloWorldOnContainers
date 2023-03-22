42ce816b74ed19b3e5a4c44e3185b35a20a0ade2
boy-eff <boyev.danila@gmail.com>
2023-03-22 12:19:09 +0300

    Add getting word collections from cache

--- src/Services/Words/Words.BusinessAccess/MediatR/Features/Collections/Queries/GetById/GetWordCollectionByIdQueryHandler.cs
+++ src/Services/Words/Words.BusinessAccess/MediatR/Features/Collections/Queries/GetById/GetWordCollectionByIdQueryHandler.cs
@@ -1,10 +1,13 @@
 ﻿using Mapster;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Shared.Exceptions;
 using Words.BusinessAccess.Contracts;
 using Words.BusinessAccess.Dtos.WordCollection;
+using Words.BusinessAccess.Extensions;
 using Words.DataAccess;
+using Words.DataAccess.Models;
 
 namespace Words.BusinessAccess.MediatR.Features.Collections.Queries.GetById;
 
@@ -12,27 +15,35 @@ public class GetWordCollectionByIdQueryHandler : IRequestHandler<GetWordCollecti
 {
     private readonly WordsDbContext _dbContext;
     private readonly IViewsCounterService _viewsCounterService;
+    private readonly IDistributedCache _cache;
 
-    public GetWordCollectionByIdQueryHandler(WordsDbContext dbContext, IViewsCounterService viewsCounterService)
+    public GetWordCollectionByIdQueryHandler(WordsDbContext dbContext, IViewsCounterService viewsCounterService, IDistributedCache cache)
     {
         _dbContext = dbContext;
         _viewsCounterService = viewsCounterService;
+        _cache = cache;
     }
 
     public async Task<WordCollectionResponseDto> Handle(GetWordCollectionByIdQuery request, CancellationToken cancellationToken)
     {
-        var collection = await _dbContext.Collections
-            .Include(x => x.Words)
-            .ThenInclude(x => x.Translations)
-            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+        var wordCollectionName = nameof(WordCollection);
+        WordCollection collection;
+        var isCacheHit = _cache.TryGetWordCollection(request.Id, out collection);
 
+        if (!isCacheHit)
+        {
+            collection = await _dbContext.Collections
+                .Include(x => x.Words)
+                .ThenInclude(x => x.Translations)
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+        }
+        
         if (collection is null)
         {
             throw new NotFoundException("Collection is not found");
         }
         
         _viewsCounterService.IncrementViewsInCollection(collection.Id);
-
         return collection.Adapt<WordCollectionResponseDto>();
     }
 }
