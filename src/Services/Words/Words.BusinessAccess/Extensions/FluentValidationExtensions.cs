f9786e34821b909844485353a349896ce25263cd
boy-eff <boyev.danila@gmail.com>
2023-03-23 14:59:35 +0300

    Add throwing custom validation exception

---
+++ src/Services/Words/Words.BusinessAccess/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using Shared.Exceptions;
+
+namespace Words.BusinessAccess.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static void ValidateAndThrowCustomException<T>(this IValidator<T> validator, T instance, ILogger logger = null)
+    {
+        var res = validator.Validate(instance);
+        
+        if (!res.IsValid)
+        {
+            logger?.LogInformation("Validation failed with errors: {ValidationErrors}", res.Errors);
+            throw new CustomValidationException(res.Errors);
+        }
+    }
+}
