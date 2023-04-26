054de9b57513a1975c0aa5c41bddb0a5743145e2
boy-eff <boyev.danila@gmail.com>
2023-04-26 17:27:14 +0300

    Add dictionary module

---
+++ client/angular/HelloWorldClient/src/app/modules/dictionary/dictionary-page/dictionary-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary-page',
+  templateUrl: './dictionary-page.component.html',
+  styleUrls: ['./dictionary-page.component.scss']
+})
+export class DictionaryPageComponent {
+
+}
---
+++ client/angular/HelloWorldClient/src/app/modules/dictionary/dictionary.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    DictionaryPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DictionaryModule { }
