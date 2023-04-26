f2ac2b8b1265a6d717da975d8ec7fddfe4618f0e
boy-eff <boyev.danila@gmail.com>
2023-04-26 19:35:06 +0300

    Delete unused files

--- src/Services/Achievements/Achievements.Application/Contracts/IUserService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Achievements.Application.Contracts;
-
-public interface IUserService
-{
-    Task UpdateAchievementPointsAsync(int userId, IUserAchievementIncrementor achievementIncrementor);
-}
--- src/Services/Words/Words.BusinessAccess/Dtos/WordTranslationCreateDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Words.BusinessAccess.Dtos;
-
-public class WordTranslationCreateDto
-{
-    public string Translation { get; set; }
-}
