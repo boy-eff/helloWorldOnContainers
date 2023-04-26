6886ea471c274e8719fe2df904f8d03d9bf1ae7c
boy-eff <boyev.danila@gmail.com>
2023-04-26 19:36:20 +0300

    Add jwt interceptor

--- client/angular/HelloWorldClient/src/app/shared/interceptors/jwt.interceptor.ts
+++ client/angular/HelloWorldClient/src/app/shared/interceptors/jwt.interceptor.ts
@@ -4,26 +4,72 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpErrorResponse,
+  HttpStatusCode,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { TokenResponse } from '../contracts/tokenResponse';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   constructor(private authService: AuthenticationService) {}
 
   intercept(
-    request: HttpRequest<unknown>,
+    request: HttpRequest<any>,
     next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    let tokenModel = this.authService.getToken();
-    if (tokenModel) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${tokenModel.accessToken}`,
-        },
-      });
+  ): Observable<HttpEvent<any>> {
+    const tokenModel = this.authService.getToken();
+    if (!tokenModel) {
+      return next.handle(request);
     }
-    return next.handle(request);
+
+    const newRequest = this.addAuthorizationHeader(
+      request,
+      tokenModel.accessToken
+    );
+    return next.handle(newRequest).pipe(
+      catchError((err) => {
+        if (
+          err instanceof HttpErrorResponse &&
+          err.status === HttpStatusCode.Unauthorized &&
+          tokenModel
+        ) {
+          return this.refreshAccessToken(request, next);
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+
+  private refreshAccessToken(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<any> {
+    return this.authService.refreshAccessToken().pipe(
+      switchMap((token: TokenResponse | null) => {
+        if (token) {
+          const newRequest = this.addAuthorizationHeader(
+            request,
+            token.access_token
+          );
+          return next.handle(newRequest);
+        }
+        return of(null);
+      }),
+      catchError((error) => throwError(() => error))
+    );
+  }
+
+  private addAuthorizationHeader(
+    request: HttpRequest<any>,
+    accessToken: string
+  ): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
   }
 }
