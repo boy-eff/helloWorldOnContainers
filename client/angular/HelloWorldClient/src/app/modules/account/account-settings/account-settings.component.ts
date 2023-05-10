6a17abb18fcb2d2296c83fbfb71326db7453486f
boy-eff <boyev.danila@gmail.com>
2023-05-10 13:13:52 +0300

    Update account settings component

--- client/angular/HelloWorldClient/src/app/modules/account/account-settings/account-settings.component.ts
+++ client/angular/HelloWorldClient/src/app/modules/account/account-settings/account-settings.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { User } from 'src/app/shared/contracts/user';
 import { EnglishLevel } from 'src/app/shared/enums/englishLevel';
+import { ChangePasswordModalComponent } from './change-password-modal/change-password-modal.component';
+import { AccountService } from 'src/app/services/account.service';
+import { switchMap } from 'rxjs';
+import {
+  EnglishLevelOption,
+  englishLevels,
+} from 'src/app/shared/contracts/englishLevelOption';
 
 @Component({
   selector: 'app-account-settings',
@@ -10,24 +17,45 @@ import { EnglishLevel } from 'src/app/shared/enums/englishLevel';
   styleUrls: ['./account-settings.component.scss'],
 })
 export class AccountSettingsComponent implements OnInit {
-  englishLevels = EnglishLevel;
+  englishLevels: EnglishLevelOption[] = englishLevels;
   user: User | null;
 
-  constructor(private authService: AuthenticationService) {}
+  englishLevel: EnglishLevel;
+
+  constructor(
+    private authService: AuthenticationService,
+    private modalService: NgbModal,
+    private accountService: AccountService
+  ) {}
   ngOnInit(): void {
     this.authService.currentUser$.subscribe((user) => {
-      console.log(user);
       this.user = user;
+      if (user) {
+        console.log(user.englishLevel);
+        this.englishLevel = user.englishLevel;
+      }
     });
   }
 
-  form = new FormGroup({
-    password: new FormControl(''),
-    englishLevel: new FormControl(EnglishLevel.Elementary),
-    image: new FormControl(null),
-  });
+  openChangePasswordModal(): void {
+    const modalRef = this.modalService.open(ChangePasswordModalComponent);
+    modalRef.result.then(
+      (result) => {},
+      (reason) => {}
+    );
+  }
 
-  onSubmit() {
-    console.log(this.form.value);
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.accountService
+        .changeImage(file)
+        .pipe(
+          switchMap(() => {
+            return this.authService.refreshUser();
+          })
+        )
+        .subscribe((x) => {});
+    }
   }
 }
